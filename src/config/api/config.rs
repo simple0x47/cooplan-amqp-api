ff076b6be5200f83307e59a38b327085813ae098
Gabriel Ami <gabriel@simpleg.eu>
2022-12-26 21:39:53 +0200

    + insert output elements into config

--- src/config/api/config.rs
+++ src/config/api/config.rs
@@ -1,30 +1,47 @@
+use serde::{Deserialize, Serialize};
+
 use crate::error::{Error, ErrorKind};
 
-use super::input_element_config::InputElementConfig;
+use super::{input_element_config::InputElementConfig, output_element_config::OutputElementConfig};
 
+#[derive(Deserialize, Serialize)]
 pub struct Config {
-    elements: Vec<InputElementConfig>,
+    input: Vec<InputElementConfig>,
+    output: Vec<OutputElementConfig>,
 }
 
 impl Config {
-    pub fn new(elements: Vec<InputElementConfig>) -> Config {
-        Config { elements }
+    pub fn input_element_configs(&self) -> &[InputElementConfig] {
+        self.input.as_slice()
     }
 
-    pub fn api_items(&self) -> &[InputElementConfig] {
-        self.elements.as_slice()
-    }
-
-    pub fn try_get_api_item(&self, name: &str) -> Result<InputElementConfig, Error> {
-        for api_item in self.elements.as_slice() {
-            if api_item.name().eq(name) {
-                return Ok(api_item.clone());
+    pub fn try_get_input_element_config(&self, name: &str) -> Result<InputElementConfig, Error> {
+        for input_element in self.input.as_slice() {
+            if input_element.name().eq(name) {
+                return Ok(input_element.clone());
             }
         }
 
         Err(Error::new(
             ErrorKind::InternalFailure,
-            format!("api item '{}' could not be found", name),
+            format!("input element '{}' could not be found", name),
+        ))
+    }
+
+    pub fn output_element_configs(&self) -> &[OutputElementConfig] {
+        self.output.as_slice()
+    }
+
+    pub fn try_get_output_element_config(&self, name: &str) -> Result<OutputElementConfig, Error> {
+        for output_element in self.output.as_slice() {
+            if output_element.name().eq(name) {
+                return Ok(output_element.clone());
+            }
+        }
+
+        Err(Error::new(
+            ErrorKind::InternalFailure,
+            format!("output element '{}' could not be found", name),
         ))
     }
 }
@@ -42,15 +59,15 @@ pub fn try_generate_config() -> Result<Config, Error> {
         }
     };
 
-    let api_items = match serde_json::from_str::<Vec<InputElementConfig>>(api_string.as_str()) {
-        Ok(api_items) => api_items,
+    let config = match serde_json::from_str::<Config>(api_string.as_str()) {
+        Ok(config) => config,
         Err(error) => {
             return Err(Error::new(
                 ErrorKind::AutoConfigFailure,
-                format!("failed to deserialize api items: {}", error),
+                format!("failed to deserialize input elements: {}", error),
             ));
         }
     };
 
-    Ok(Config::new(api_items))
+    Ok(config)
 }
